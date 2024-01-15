0069de457b6cae284fe8011b979b4358ccf5d124
MDKMauricioKlainbard <mauricioklainbard01@gmail.com>
2024-01-15 18:03:38 -0300

    Added MongoDB feature and GlobalValidationPipe.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+
+const MONGO_URI_DB : string = process.env.MONGO_URI_DB;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_URI_DB)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT: number = Number(process.env.PORT)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
 }
 bootstrap();
